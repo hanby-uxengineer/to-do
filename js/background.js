8fcb31e9126d3fff4ce26a27004ee7f63b8cd014
hanby-uxengineer <yihaanstar@gmail.com>
2022-04-01 23:59:45 +0900

    Update background.js

---
+++ js/background.js
@@ -0,0 +1,9 @@
+const images =  [
+    "https://images.unsplash.com/photo-1648664846181-19b243692714?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80",
+    "https://images.unsplash.com/photo-1648573710616-1961b2f9d6b0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80",
+    "https://images.unsplash.com/photo-1648625523422-50d4d3b5089b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"];
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+const bgImage = document.createElement("img");
+bgImage.src = chosenImage;
+
+document.body.prepend(bgImage); // appendChild
