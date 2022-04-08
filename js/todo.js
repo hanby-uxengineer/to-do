764a99ba9bb6d343f2504244742b175074ff5f0a
hanby-uxengineer <yihaanstar@gmail.com>
2022-04-08 23:36:19 +0900

    delete todo function

--- js/todo.js
+++ js/todo.js
@@ -12,17 +12,20 @@ function saveToDos() {
 function deleteToDo(event) {
     const li = event.target.parentElement;
     li.remove();
+    toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+    saveToDos();
 }
 
 function paintToDo(newToDo) {
     const li = document.createElement("li");
+    li.id = newToDo.id;
     const span = document.createElement("span");
     li.appendChild(span);
     const button = document.createElement("button");
     button.innerText = "✕";
     button.addEventListener("click", deleteToDo);
     li.appendChild(button);
-    span.innerText = newToDo;
+    span.innerText = newToDo.text;
     toDoList.appendChild(li);
 }
 
@@ -30,8 +33,12 @@ function handleToDoSubmit(event) {
     event.preventDefault();
     const newToDo = toDoInput.value;
     toDoInput.value = "";
-    toDos.push(newToDo);
-    paintToDo(newToDo);
+    const newToDoObj = {
+        text: newToDo,
+        id: Date.now(),
+    }
+    toDos.push(newToDoObj);
+    paintToDo(newToDoObj);
     saveToDos();
 }
 
